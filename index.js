83bc8cee822184669d5eaf3f9151c26e385c6922
Hoa Le <Hoa_le9485@yahoo.com>
2021-10-13 21:40:27 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function add (c,d) {
+    return c + d;
+}
+function subtract (c,d) {
+    return c - d;
+}function multiply (c,d) {
+    return c * d;
+}function divide (c,d) {
+    return c / d;
+}
+function increment (n) {
+    return n+= 1;
+}
+function decrement (n) {
+    return n-= 1;
+}function makeInt (n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+    
+}
